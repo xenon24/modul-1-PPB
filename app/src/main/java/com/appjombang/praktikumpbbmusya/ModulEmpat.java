281e860536ddbe62125543fbe6eba28945c98d7c
Muhammad Syaiful Rizal <you@example.com>
2018-04-18 09:40:16 +0700

    perubahan modul 4

--- app/src/main/java/com/appjombang/praktikumpbbmusya/ModulEmpat.java
+++ app/src/main/java/com/appjombang/praktikumpbbmusya/ModulEmpat.java
@@ -1,31 +1,17 @@
 package com.appjombang.praktikumpbbmusya;
 
-
 import android.os.Bundle;
 import android.provider.ContactsContract;
 import android.app.ListActivity;
 import android.database.Cursor;
 import android.view.View;
 import android.widget.Button;
-import android.widget.ListView;
 import android.widget.SimpleCursorAdapter;
 import android.widget.Toast;
 
 public class ModulEmpat extends ListActivity {
 
-    @Override
-    public long getSelectedItemId() {
-        return super.getSelectedItemId();
-    }
-
-    @Override
-    public int getSelectedItemPosition() {
-        return super.getSelectedItemPosition();
-    }
-
-    ListView listView;
     Cursor cursor;
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -44,13 +30,8 @@ public class ModulEmpat extends ListActivity {
                 int[] to = {android.R.id.text1, android.R.id.text2};
                 SimpleCursorAdapter listadapter = new SimpleCursorAdapter(ModulEmpat.this, android.R.layout.simple_list_item_2, cursor, from, to);
                 setListAdapter(listadapter);
-
-                listView = getListView();
-                listView.setChoiceMode(ListView.CHOICE_MODE_MULTIPLE);
                 Toast.makeText(ModulEmpat.this, "Seluruh Kontak Anda Berhasil Ditampilkan.", Toast.LENGTH_SHORT).show();
             }
         } );
-
-
     }
 }
